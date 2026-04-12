e09d87b9bd1aed8419cb2019882177ab6f5cde0b
Hashem <halfai9234@gmail.com>
2026-04-12 17:36:37 +0300

    Adding fonts

--- Control.gd
+++ Control.gd
@@ -2,8 +2,21 @@ extends Control
 var allocation_points = 50
 var stats = {"luck":1,"strength":1,"speed":1,"endurance":1,"wit":1}
 @onready var label = $Label
-func _ready():
-	label.
+var PLace_in_story = -1
 func _input(event):
 	if event.is_action_pressed("ACTION"):
-		label.text = "CHOOSE YOUR STATS"
+		PLace_in_story += 1
+	match PLace_in_story:
+		0: text("HEllo")
+			
+		1: text("Welcome")
+			
+		2: text("Good eving")
+			
+		3: pass
+			
+		5: pass
+			
+		
+func text(text):
+	label.text = text
