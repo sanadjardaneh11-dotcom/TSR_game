94bc23f2af7a279ae7e165caaef9576c0c86400a
Hashem <halfai9234@gmail.com>
2026-04-18 23:41:01 +0300

    Completing the save system

--- Scripts/settings.gd
+++ Scripts/settings.gd
@@ -3,12 +3,12 @@ extends Control
 @onready var values: VBoxContainer = $HBoxContainer/Values
 @onready var display_value: VBoxContainer = $"HBoxContainer/Display value"
 var savepath = "res://savedata.json"
-var settings:Dictionary
+var savedata:Dictionary
 var numofsettings = 2
 func _ready() -> void:
 	load_json_file()
-	values.get_child(0).value = settings["settings"][0]
-	values.get_child(1).value = settings["settings"][1]
+	values.get_child(0).value = savedata["settings"][0]
+	values.get_child(1).value = savedata["settings"][1]
 	for num in numofsettings:
 		display_value.get_child(num).text = str(values.get_child(num).value)
 
@@ -17,9 +17,16 @@ func load_json_file():
 	var json = file.get_as_text()
 	var jsonobject = JSON.new()
 	jsonobject.parse(json)
-	settings = jsonobject.data
-	return settings
+	savedata = jsonobject.data
+	return savedata
+
+func save_to_json_file():
+	var file = FileAccess.open(savepath, FileAccess.WRITE)
+	var json_text = JSON.stringify(savedata)
+	file.store_string(json_text)
 
 func _on_value_changed(value: float) -> void:
 	for num in numofsettings:
 		display_value.get_child(num).text = str(values.get_child(num).value) 
+		savedata["settings"][num] = values.get_child(num).value
+	save_to_json_file()
