6651740a7255649ee7e7d95e2063951e890c037b
Hashem <halfai9234@gmail.com>
2026-04-21 14:41:39 +0300

    Fixing bugs, making better ui and save system is fully avalable

--- Scripts/settings.gd
+++ Scripts/settings.gd
@@ -1,32 +1,49 @@
 extends Control
 
-@onready var values: VBoxContainer = $HBoxContainer/Values
-@onready var display_value: VBoxContainer = $"HBoxContainer/Display value"
+@onready var music: HSlider = $settings/SliderSettings/Values/Music
+@onready var sounds: HSlider = $settings/SliderSettings/Values/Sounds
+@onready var dispmusic: Label = $"settings/SliderSettings/Display value/Music"
+@onready var dispsounds: Label = $"settings/SliderSettings/Display value/Sounds"
 var savepath = "res://savedata.json"
 var savedata:Dictionary
-var numofsettings = 2
+
 func _ready() -> void:
 	load_json_file()
-	values.get_child(0).value = savedata["settings"][0]
-	values.get_child(1).value = savedata["settings"][1]
-	for num in numofsettings:
-		display_value.get_child(num).text = str(values.get_child(num).value)
+	music.value = savedata["settings"][0]
+	print("Loaded value"+str(savedata["settings"][0])+"to music")
+	sounds.value = savedata["settings"][1]
+	print("Loaded value"+str(savedata["settings"][1])+"to sounds")
+	dispmusic.text = str(music.value)
+	dispsounds.text = str(sounds.value)
+	
 
 func load_json_file():
 	var file = FileAccess.open(savepath, FileAccess.READ)
 	var json = file.get_as_text()
 	var jsonobject = JSON.new()
 	jsonobject.parse(json)
+	print("Loaded:"+str(jsonobject.data)+"from file")
 	savedata = jsonobject.data
 	return savedata
 
 func save_to_json_file():
-	var file = FileAccess.open(savepath, FileAccess.WRITE)
+	var file = FileAccess.open(savepath, FileAccess.ModeFlags.WRITE)
 	var json_text = JSON.stringify(savedata)
+	print("written:"+json_text+"to file")
 	file.store_string(json_text)
 
-func _on_value_changed(value: float) -> void:
-	for num in numofsettings:
-		display_value.get_child(num).text = str(values.get_child(num).value) 
-		savedata["settings"][num] = values.get_child(num).value
+func _on_music_value_changed(value: float) -> void:
+	dispmusic.text = str(music.value) 
+	savedata["settings"][0] = music.value
+
+func _on_sounds_value_changed(value: float) -> void:
+	dispsounds.text = str(sounds.value) 
+	savedata["settings"][1] = sounds.value
+
+
+func _on_apply_pressed() -> void:
 	save_to_json_file()
+
+
+func _on_quit_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Starting screen.tscn")
