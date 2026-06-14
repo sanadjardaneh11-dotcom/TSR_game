e41f98cf0cf547c69ed8b85c57f90e1361d67da3
hashem <halfai9234@gmail.com>
2026-06-14 19:54:31 +0300

    Adding book reading funcitonallity and removing rpc scene

--- Scripts/player_character.gd
+++ Scripts/player_character.gd
@@ -7,6 +7,10 @@ extends CharacterBody3D
 @onready var tree = $AnimationPlayer/AnimationTree
 @onready var state = $AnimationPlayer/AnimationTree.get("parameters/playback")
 @onready var mesh: Node3D = $Armature
+@onready var book_detector: RayCast3D = $SpringArm3D/Camera3D/book_detector
+@onready var arrow: TextureRect = $CanvasLayer/arrow/TextureRect
+const ARROW = preload("uid://dpltywfej40xp")
+const ARROW_SELECTED = preload("uid://bgrsvh3jk7p2e")
 
 var velocity_on_a_plane = Vector2(0,0)
 
@@ -15,6 +19,9 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
+	if book_detector.is_colliding() == true:
+		arrow.texture = ARROW_SELECTED
+	else:arrow.texture = ARROW
 	velocity_on_a_plane = Input.get_vector("ui_left","ui_right","ui_up","ui_down")*speed
 	velocity += Vector3(velocity_on_a_plane.x,0,velocity_on_a_plane.y).rotated(Vector3.UP, camera.rotation.y)*delta
 	velocity = Vector3(clampf(velocity.x,-max_speed,max_speed),velocity.y,clampf(velocity.z,-max_speed,max_speed))
@@ -23,10 +30,17 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 
+func _input(event: InputEvent) -> void:
+	if event.is_action("ui_accept"):
+		if book_detector.is_colliding() == true:
+			book_read()
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		camera.rotation.x -= event.relative.y * camera_speed 
-		camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, -30,85) 
+		camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, -90,90) 
 		camera.rotation.y -= event.relative.x * camera_speed 
 		mesh.rotation.y = camera.rotation.y
 	
+func book_read():
+	print("read book")
